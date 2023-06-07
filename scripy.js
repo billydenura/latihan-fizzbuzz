36053b9e63721c67671f42b7d8a08c7ae479480c
Muhammad Billy Denura <96166348+billydenura@users.noreply.github.com>
2023-06-07 20:55:04 +0700

    Add files via upload

--- scripy.js
+++ scripy.js
@@ -6,8 +6,17 @@ btn[0].addEventListener("click",function(){
     let number = document.getElementById("masukan").value
     const hasil = document.createElement("p")
     console.log(number)
-    if(number%3==0){
+    if(number%3==0 && number%5==0){
+        hasil.innerHTML = "FizzBuzz"
+        keluaran[0].append(hasil);
+    }else if(number%5==0){
+        hasil.innerHTML = "Buzz"
+        keluaran[0].append(hasil);
+    }else if(number%3==0){
         hasil.innerHTML = "Fizz"
-        keluaran.append(hasil);
+        keluaran[0].append(hasil);
+    }else{
+        hasil.innerHTML = number
+        keluaran[0].append(hasil);
     }
 })
